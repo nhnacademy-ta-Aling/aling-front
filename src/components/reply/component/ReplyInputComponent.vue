2611a1550c331e9984eecc73a54dcbcb4e482c96
devel0624 <devel062@gmail.com>
2024-03-05 17:59:03 +0900

    [Feat] 게시물 조회 뷰 통합 및 댓글 조회 추가
    - 일반 게시물과 그룹 게시물 조회 url 을 통합하였습니다.
    - 댓글 조회 추가하였습니다.

---
+++ src/components/reply/component/ReplyInputComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data: () => ({
+    rules: [
+      (value) => !!value || "입력 칸이 비어있어요.",
+      (value) => (value && value.length >= 2) || "길이가 너무 짧아요",
+    ],
+  }),
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+  },
+};
+</script>
+
+<template>
+  <form>
+    <v-text-field
+      label="게시물에 댓글을 달아보세요"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <button type="submit" class="mr-4 v-btn--icon">submit</button>
+  </form>
+</template>
---
+++ src/pages/post/component/PostBottomComponent.vue
@@ -0,0 +1,35 @@
+<script>
+import ReplyListModal from "@/components/reply/ReplyListModal.vue";
+
+export default {
+  components: { ReplyListModal },
+  data() {
+    return {
+      isReplyViewed: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <v-bottom-navigation grow>
+    <v-btn>
+      <span>좋아요</span>
+      <v-icon>mdi-thumb-up-outline</v-icon>
+    </v-btn>
+    <v-btn>
+      <span>스크랩</span>
+      <v-icon>mdi-bookmark-outline</v-icon>
+    </v-btn>
+    <v-btn @click="isReplyViewed = true">
+      <span>댓글</span>
+      <v-icon>mdi-message-reply-text-outline</v-icon>
+    </v-btn>
+    <v-overlay v-if="isReplyViewed">
+      <v-layout>
+        <button @click="isReplyViewed = false">댓글창 닫기</button>
+      </v-layout>
+      <reply-list-modal />
+    </v-overlay>
+  </v-bottom-navigation>
+</template>
