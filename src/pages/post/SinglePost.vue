c7838c0932484ccf7c7464161b509b0cbc7c4d56
Gyeongseo Bak <rudtj2966@gmail.com>
2024-03-09 17:23:18 +0900

    [Refactor] : 게시글 수정 임시 추가

--- src/pages/post/SinglePost.vue
+++ src/pages/post/SinglePost.vue
@@ -1,5 +1,5 @@
 <script>
-import { Viewer } from "@toast-ui/vue-editor";
+import { Editor, Viewer } from "@toast-ui/vue-editor";
 
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
@@ -11,6 +11,7 @@ import "prismjs/themes/prism-solarizedlight.css";
 
 export default {
   components: {
+    Editor,
     Viewer,
   },
   data() {
@@ -20,6 +21,8 @@ export default {
       },
       postData: null,
       contentMore: false,
+      postDeleteModal: false,
+      postModifyModal: false,
     };
   },
   created() {
@@ -41,6 +44,29 @@ export default {
     },
     alertReport() {
       console.log("신고");
+    },
+    openPostDeleteModal() {
+      this.postDeleteModal = true;
+    },
+    closePostDeleteModal() {
+      this.postDeleteModal = false;
+    },
+    clickPostDelete() {
+      this.$axios
+        .delete("/post/api/v1/band-posts/" + this.$route.params.postNo)
+        .then(() => {
+          this.closePostDeleteModal();
+        })
+        .catch(() => {
+          console.log("server error");
+          this.closePostDeleteModal();
+        });
+    },
+    openPostModifyModal() {
+      this.postModifyModal = true;
+    },
+    closePostModifyModal() {
+      this.postModifyModal = false;
     },
     toggleContentMore() {
       this.contentMore = !this.contentMore;
@@ -107,6 +133,18 @@ export default {
                 </v-list-item-icon>
                 <v-list-item-title>신고</v-list-item-title>
               </v-list-item>
+              <v-list-item @click="openPostModifyModal">
+                <v-list-item-icon>
+                  <v-icon>mdi-pencil-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>게시글 수정</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="openPostDeleteModal">
+                <v-list-item-icon>
+                  <v-icon>mdi-trash-can-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>삭제</v-list-item-title>
+              </v-list-item>
             </v-list>
           </v-list>
         </v-menu>
@@ -162,6 +200,84 @@ export default {
           <v-icon small>mdi-message-reply-text-outline</v-icon>
         </v-btn>
       </v-bottom-navigation>
+      <!-- 게시글 수정 모달 시작 -->
+      <v-dialog v-model="postModifyModal" max-width="1000">
+        <v-card>
+          <v-card-title class="text-sm-h5">게시글 수정</v-card-title>
+          <v-divider></v-divider>
+          <v-divider></v-divider>
+          <br />
+          <v-container>
+            <div class="post-body">
+              <v-row>
+                <v-col cols="9">
+                  <v-text-field
+                    :value="postData.post.title"
+                    :error-messages="postTitleValidMsg"
+                    :counter="50"
+                    label="게시글 제목"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="3" class="pt-7">
+                  <v-select
+                    :items="items"
+                    dense
+                    label="게시글 Category"
+                  ></v-select>
+                </v-col>
+              </v-row>
+
+              <br />
+              <br />
+              <Editor
+                ref="tuiEditor"
+                height="300px"
+                initialEditType="wysiwyg"
+                :initialValue="postData.post.content"
+                :options="editorOptions"
+                @change="getEditorInput"
+              ></Editor>
+              <div class="v-text-field__details">
+                <div class="v-messages theme--light error--text" role="alert">
+                  <div class="v-messages__message editor-error-message">
+                    <p v-if="postContentValidMsg" class="error--text">
+                      {{ postContentValidMsg }}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </v-container>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="closePostModifyModal">
+              취소
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="clickPostDelete">
+              수정
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- 게시글 수정 모달 끝 -->
+      <!-- 게시글 삭제 모달 시작 -->
+      <v-dialog v-model="postDeleteModal" max-width="500px">
+        <v-card>
+          <v-card-text class="pt-4 font-weight-bold">
+            게시글을 삭제하면 복구할 수 없습니다. 삭제하시겠습니까?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="closePostDeleteModal">
+              취소
+            </v-btn>
+            <v-btn color="red darken-1" text @click="clickPostDelete">
+              삭제
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- 게시글 삭제 모달 끝 -->
     </v-responsive>
   </v-sheet>
 </template>
@@ -186,5 +302,9 @@ export default {
 
 .title {
   padding: 2% 2%;
+}
+
+.post-body {
+  padding: 0 4.5%;
 }
 </style>
